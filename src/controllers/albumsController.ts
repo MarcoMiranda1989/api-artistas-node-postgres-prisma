13c3da2e4f5d4ce3961ee015446d8006e34cdb4a
MarcoMiranda1989 <marck.cena1@gmail.com>
2025-08-15 21:51:50 -0600

    cambios en los schemas de prisma y en una nueva implementacion de prisma

--- src/controllers/albumsController.ts
+++ src/controllers/albumsController.ts
@@ -1,9 +1,9 @@
 import type { Request, Response } from "express";
-import prisma from "../models/albums.js"
+import prisma from "../models/prisma.js"
 
 export const createAlbum = async (req:Request, res: Response): Promise<void> => {
     try {
-        const { titulo, anio, genero, artistaId, artista, canciones,createdAt, updatedAt} = req.body
+        const { titulo, anio, genero, artistaId, artista, canciones, createdAt, updatedAt } = req.body
 
         if (!titulo) {
             res.status(400).json({ error: "El titulo es obligatorio" });
@@ -21,12 +21,8 @@ export const createAlbum = async (req:Request, res: Response): Promise<void> => 
             res.status(400).json({ error: "El ID del artista es obligatorio" });
             return;
         }
-       
 
-
-        //checando todos los errores podemos continuar a crear el album
-
-        const album = await prisma.create({
+        const album = await prisma.album.create({
             data:{
                 titulo,
                 anio,
@@ -38,49 +34,40 @@ export const createAlbum = async (req:Request, res: Response): Promise<void> => 
         })
         res.status(201).json({ album });
     } catch (error:any) {
-        {
         if (error?.code === "P2002" && error?.meta?.target?.includes("artistaId")) {
-      res.status(400).json({ message: "el artista ingresado ya existe" });
-    }
-    console.log(error);
-    res.status(500).json({ error: "Hubo un error" });
-    }
-        
+            res.status(400).json({ message: "el artista ingresado ya existe" });
+        } else {
+            console.log(error);
+            res.status(500).json({ error: "Hubo un error" });
+        }
     }
 }
 
 export const getAllAlbums = async (req:Request, res: Response): Promise<void> => {
-try {
-    const albums= await prisma.findMany({
-            include: {
-                canciones: true
-                 }
-            
-    })
-    res.status(200).json(albums)
-} catch (error:any) {
-    console.log(error);
-    res.status(500).json({ error: "Hubo un error" });
-}
+    try {
+        const albums= await prisma.album.findMany({
+            include: { canciones: true }
+        })
+        res.status(200).json(albums)
+    } catch (error:any) {
+        console.log(error);
+        res.status(500).json({ error: "Hubo un error" });
+    }
 }
 
 export const getAlbumById = async (req:Request, res: Response): Promise<void> => {
     const albumId = Number(req.params.id);
     try {
-        const album = await prisma.findUnique({
-            where:{
-                id: albumId
-            },
-                  include: {
-                    canciones: true
-                    }  
+        const album = await prisma.album.findUnique({
+            where:{ id: albumId },
+            include: { canciones: true }  
         })
 
-    if (!album) {
-        res.status(404).json({ error: "la cancion no fue encontrada" });
-        return;
-    }
-    res.status(200).json(album);
+        if (!album) {
+            res.status(404).json({ error: "la cancion no fue encontrada" });
+            return;
+        }
+        res.status(200).json(album);
     } catch (error:any) {
         console.log(error);
         res.status(500).json({ error: "Hubo un error" });
@@ -89,60 +76,42 @@ export const getAlbumById = async (req:Request, res: Response): Promise<void> =>
 
 export const updateAlbum = async (req:Request, res: Response): Promise<void> => {
     const idAlbum = Number(req.params.id);
-    const { titulo, anio, genero, artistaId, artista  } = req.body
+    const { titulo, anio, genero, artistaId, artista } = req.body
     try {
-        //aqui nos traemos toda la info del req.body con el spread operator listo para actualizarse
         let albumData:any = {...req.body}
-        if (titulo) {
-            albumData.titulo=titulo
-        }
-         if (anio) {
-            albumData.anio=anio
-        }
-         if (genero) {
-            albumData.genero=genero
-        }
-         if (artistaId) {
-            albumData.artistaId=artistaId
-        }
-         if (artista) {
-            albumData.artista=artista
-        }
+        if (titulo) albumData.titulo = titulo
+        if (anio) albumData.anio = anio
+        if (genero) albumData.genero = genero
+        if (artistaId) albumData.artistaId = artistaId
+        if (artista) albumData.artista = artista
 
-        const song = await prisma.update({
-            where:{
-                id:idAlbum
-            },
-            data:albumData
+        const song = await prisma.album.update({
+            where:{ id: idAlbum },
+            data: albumData
         })
         res.status(200).json(song);
     } catch (error:any) {
         if (error?.code === "P2002" && error?.meta?.target?.includes("titulo")) {
-      res.status(400).json({ error: "El album ingresado ya existe" });
-    } else if (error?.code === "P2025") {
-      res.status(400).json({ error: "album no encontrado" });
-    } else {
-      console.log(error);
-      res.status(500).json({ error: "Hubo un error, prueba mas tarde" });
-    }
+            res.status(400).json({ error: "El album ingresado ya existe" });
+        } else if (error?.code === "P2025") {
+            res.status(400).json({ error: "album no encontrado" });
+        } else {
+            console.log(error);
+            res.status(500).json({ error: "Hubo un error, prueba mas tarde" });
+        }
     }
 }
 
 export const deleteAlbum = async (req:Request, res: Response): Promise<void> => {
     const idAlbum = Number(req.params.id);
-      try {
-    await prisma.delete({
-      where: {
-        id: idAlbum,
-      },
-    });
+    try {
+        await prisma.album.delete({
+            where: { id: idAlbum },
+        });
 
-    res
-      .status(200)
-      .json({ message: `El album ${idAlbum} ha sido eliminado con exito` })
-      .end();
-  } catch (error: any) {
-    console.log(error);
-    res.status(500).json({ error: "Hubo un error, prueba mas tarde" });
-  }
+        res.status(200).json({ message: `El album ${idAlbum} ha sido eliminado con exito` }).end();
+    } catch (error: any) {
+        console.log(error);
+        res.status(500).json({ error: "Hubo un error, prueba mas tarde" });
+    }
 }
--- src/controllers/songsController.ts
+++ src/controllers/songsController.ts
@@ -1,62 +1,58 @@
 import type { Request, Response } from "express";
-import prisma from '../models/songs.js'
-
+import prisma from '../models/prisma.js'  // <-- import del cliente completo
 
 export const createSong = async (req:Request, res: Response): Promise<void> => {
     const {titulo, duracion, albumId} = req.body
-   try {
-     if(!titulo){
-        res.status(400).json({error: 'El titulo debe estar incluido'})
-    }
-    if(!duracion){
-        res.status(400).json({error: 'La duracion debe estar incluido'})
-    }
-    if(!albumId){
-        res.status(400).json({error: 'El albumId debe estar incluido'})
-    }
+    try {
+        if(!titulo){
+            res.status(400).json({error: 'El titulo debe estar incluido'})
+            return;
+        }
+        if(!duracion){
+            res.status(400).json({error: 'La duracion debe estar incluido'})
+            return;
+        }
+        if(!albumId){
+            res.status(400).json({error: 'El albumId debe estar incluido'})
+            return;
+        }
 
-    //comprobando que la informacion esta correcta, podemos crear la cancion
-
-    const song = await prisma.create({
-        data:{
-            titulo,
-            duracion,
-            albumId
-        },
-    }) 
-    res.status(201).json(song)
-   } catch (error:any) {
-     
-    console.log(error);
-    res.status(500).json({ error: "Hubo un error" });
-   }
+        const song = await prisma.cancion.create({
+            data:{
+                titulo,
+                duracion,
+                albumId
+            },
+        }) 
+        res.status(201).json(song)
+    } catch (error:any) {
+        console.log(error);
+        res.status(500).json({ error: "Hubo un error" });
+    }
 }
-
 
 export const getAllSongs = async (req:Request, res: Response): Promise<void> => {
-try { 
-    const songs= await prisma.findMany()
-    res.status(200).json(songs)
-} catch (error:any) {
-    console.log(error);
-    res.status(500).json({ error: "Hubo un error" });
-}
+    try { 
+        const songs = await prisma.cancion.findMany()
+        res.status(200).json(songs)
+    } catch (error:any) {
+        console.log(error);
+        res.status(500).json({ error: "Hubo un error" });
+    }
 }
 
 export const getSongById = async (req:Request, res: Response): Promise<void> => {
     const songId = Number(req.params.id);
     try {
-        const song = await prisma.findUnique({
-            where:{
-                id: songId
-            }
+        const song = await prisma.cancion.findUnique({
+            where:{ id: songId }
         })
 
-    if (!song) {
-        res.status(404).json({ error: "la cancion no fue encontrada" });
-        return;
-    }
-    res.status(200).json(song);
+        if (!song) {
+            res.status(404).json({ error: "la cancion no fue encontrada" });
+            return;
+        }
+        res.status(200).json(song);
     } catch (error:any) {
         console.log(error);
         res.status(500).json({ error: "Hubo un error" });
@@ -67,55 +63,38 @@ export const updateSong = async (req:Request, res: Response): Promise<void> => {
     const songId = Number(req.params.id);
     const {titulo, duracion, albumId} = req.body
     try {
-        //aqui nos traemos toda la info del req.body con el spread operator listo para actualizarse
         let songData:any = {...req.body}
-        if (titulo) {
-            songData.titulo=titulo
-        }
-         if (duracion) {
-            songData.duracion=duracion
-        }
-         if (albumId) {
-            songData.albumId=albumId
-        }
+        if (titulo) songData.titulo = titulo
+        if (duracion) songData.duracion = duracion
+        if (albumId) songData.albumId = albumId
 
-        const song = await prisma.update({
-            where:{
-                id:songId
-            },
-            data:songData
+        const song = await prisma.cancion.update({
+            where:{ id: songId },
+            data: songData
         })
         res.status(200).json(song);
     } catch (error:any) {
         if (error?.code === "P2002" && error?.meta?.target?.includes("titulo")) {
-      res.status(400).json({ error: "La cancion ingresado ya existe" });
-    } else if (error?.code === "P2025") {
-      res.status(400).json({ error: "cancion no encontrado" });
-    } else {
-      console.log(error);
-      res.status(500).json({ error: "Hubo un error, prueba mas tarde" });
-    }
+            res.status(400).json({ error: "La cancion ingresado ya existe" });
+        } else if (error?.code === "P2025") {
+            res.status(400).json({ error: "cancion no encontrado" });
+        } else {
+            console.log(error);
+            res.status(500).json({ error: "Hubo un error, prueba mas tarde" });
+        }
     }
 }
 
 export const deleteSong = async (req:Request, res: Response): Promise<void> => {
     const songId = Number(req.params.id);
-      try {
-    await prisma.delete({
-      where: {
-        id: songId,
-      },
-    });
+    try {
+        await prisma.cancion.delete({
+            where: { id: songId },
+        });
 
-    res
-      .status(200)
-      .json({ message: `La cancion ${songId} ha sido eliminado con exito` })
-      .end();
-  } catch (error: any) {
-    console.log(error);
-    res.status(500).json({ error: "Hubo un error, prueba mas tarde" });
-  }
+        res.status(200).json({ message: `La cancion ${songId} ha sido eliminado con exito` }).end();
+    } catch (error: any) {
+        console.log(error);
+        res.status(500).json({ error: "Hubo un error, prueba mas tarde" });
+    }
 }
-
-
-
